12ec1c47a0d013e216ddde016e26058d9dbfc8bb
kepo402 <alawodeolayinka2@gmail.com>
2024-08-09 22:43:01 +0100

    new commit

--- football_streaming_site/settings.py
+++ football_streaming_site/settings.py
@@ -93,10 +93,15 @@ WSGI_APPLICATION = 'football_streaming_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'gnWEJeETLPtlCISAClktczkKgRoMwEuI',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '29866',
     }
 }
+
 
 
 # Password validation
