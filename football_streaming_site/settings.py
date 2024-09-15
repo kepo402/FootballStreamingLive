51dda19d86d4e2f1d97249dd478240b5097f36c4
kepo402 <alawodeolayinka2@gmail.com>
2024-09-15 23:53:40 +0100

    new commit

--- football_streaming_site/settings.py
+++ football_streaming_site/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--1#8il*2ebccj=2pr2dvrebpbj3fwj)@w+8ote^7^6jm5yv#c@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost', 
